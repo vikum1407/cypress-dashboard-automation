e239568ad69d115687357fb31b6b11f98ee0f6db
vikum1407 <vikum1407@gmail.com>
2023-09-17 20:44:02 +0530

    Optimized the cypress test case code by using cypress alias

--- cypress/e2e/GW_JVMLoadClasses_Dashboard.cy.js
+++ cypress/e2e/GW_JVMLoadClasses_Dashboard.cy.js
@@ -5,37 +5,34 @@ describe('JVM Load Classes Dashboard Automation', ()=>{
 
     const apiURL = "https://prometheus.axp.com/api/v1/query?query=sum(jvm_classes_loaded_total{job=~'.*'})";
 
-    it('Status Code validation in JVM Load Classes Dashboard', () =>{
+    beforeEach(() =>{
         cy.request({
             method: 'GET',
             url: apiURL
         }).then((promRes) =>{
-            expect(promRes.status).to.eq(200);
+            cy.wrap(promRes).as('apiResponse')
         })
+    })
+
+    it('Status Code validation in JVM Load Classes Dashboard', () =>{
+        cy.get('@apiResponse').then((response) => {
+            expect(response.status).to.equal(200);
+          });
     });
 
     it('Array length validation in JVM Load Classes Dashboard', () =>{
-        cy.request({
-            method: 'GET',
-            url: apiURL
-        }).then((promRes) =>{
-            expect(promRes.status).to.eq(200);
-            const values = promRes.body.data.result[0].value;
+        cy.get('@apiResponse').then((response) =>{
+            const values = response.body.data.result[0].value;
             cy.wrap(values).should('have.length.greaterThan', 0);
         })
     });
 
     it('Array element value validation in JVM Load Classes Dashboard', () =>{
-        cy.request({
-            method: 'GET',
-            url: apiURL
-        }).then((promRes) =>{
-            expect(promRes.status).to.eq(200);
-            const values = promRes.body.data.result[0].value;
+        cy.get('@apiResponse').then((response) =>{
+            const values = response.body.data.result[0].value;
             // Assuming the integer value is at index 1 in the 'value' array
             const integerValue = parseFloat(values[1]);
-
             cy.wrap(integerValue).should('be.gt', 0);
-        })
+        })            
     });
 })
